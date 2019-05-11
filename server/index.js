40730464bb7250ae23f9f9f7d90f6caeb8e263af
Kadauchi <kadauchi@gmail.com>
2019-05-11 11:10:57 -0700

    Setup sockets.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,40 @@
+const axios = require(`axios`);
 const express = require(`express`);
+const http = require(`http`);
+const socketIo = require(`socket.io`);
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 const port = 3001;
 
-app.get(`/`, (req, res) => {
-  res.send(`Hello world`);
+let connections = 0;
+
+app.get(`*`, (req, res) => {
+  res.redirect(`http://localhost:3000/`);
 });
 
-app.listen(port, () => {
+io.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
 });
+
+io.on(`connection`, (socket) => {
+  connections++;
+  socket.emit(`count`, connections);
+  console.log(connections);
+
+  socket.on(`disconnect`, () => {
+    connections--;
+    console.log(connections);
+  });
+});
+
+async function fetchMTC() {
+  const response = await axios.get(`https://mturkcrowd.com/api.php?action=getPosts&order_by=post_date&limit=10`);
+  return response.data.posts;
+}
+
+async function fetchTVF() {
+  const response = await axios.get(`https://forum.turkerview.com/hub.php?action=getPosts&order_by=post_date&limit=10`);
+  return response.data.posts;
+}
--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import socketIoClient from 'socket.io-client';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const socket = socketIoClient(`http://127.0.0.1:3001`);
+  socket.on(`count`, (data) => {
+    console.log(data);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +17,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
