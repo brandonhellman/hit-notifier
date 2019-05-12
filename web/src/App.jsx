5a65dfcdf279fac322c2f00186fd4c771b1c228b
Kadauchi <kadauchi@gmail.com>
2019-05-11 18:27:56 -0700

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import socketIoClient from 'socket.io-client';
 
+import Header from './components/Header';
+import Layout from './components/Layout';
+
 function App() {
   const socket = socketIoClient(`http://127.0.0.1:3001`);
   socket.on(`count`, (data) => {
     console.log(data);
   });
 
-  return <div>HIT Notifier</div>;
+  return (
+    <Layout>
+      <Header />
+    </Layout>
+  );
 }
 
 export default App;
