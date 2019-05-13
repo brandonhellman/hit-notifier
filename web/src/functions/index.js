b5aa00eb9d483884d6c191e5c2ec3e1294404e08
Kadauchi <kadauchi@gmail.com>
2019-05-13 11:31:22 -0700

    Added some new functions.

---
+++ web/src/functions/index.js
@@ -0,0 +1,12 @@
+import moment from 'moment';
+import psl from 'psl';
+
+export function getDomain(url) {
+  const { hostname } = new URL(url);
+  const parsed = psl.parse(hostname);
+  return parsed.domain;
+}
+
+export function getFormattedDate(date) {
+  return moment(date).format(`LTS`);
+}
