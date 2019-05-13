b5c1d69cf593350c2e32d9357413e89fffe5091b
Kadauchi <kadauchi@gmail.com>
2019-05-13 08:18:28 -0700

    Update Latest.jsx

--- web/src/tabs/Latest.jsx
+++ web/src/tabs/Latest.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import renderHTML from 'react-render-html';
 
-function Latest() {
-  return <div>Latest</div>;
+function Latest({ hit }) {
+  return hit ? <div>{renderHTML(hit.html)}</div> : <div>No latest HITs found =(</div>;
 }
 
-export default Latest;
+function mapStateToProps(state) {
+  return {
+    hit: state.hits[0],
+  };
+}
+
+export default connect(mapStateToProps)(Latest);
