241a33f705d5dcb05ea94875b8308d362b191086
Kadauchi <kadauchi@gmail.com>
2019-05-15 12:20:55 -0700

    Update store.js

--- web/src/redux/store.js
+++ web/src/redux/store.js
@@ -4,11 +4,26 @@ import storage from 'redux-persist/lib/storage';
 import { createLogger } from 'redux-logger';
 
 import reducers from './reducers';
+import { initialState as initialSettings } from './reducers/settings';
 
 const persistConfig = {
-  key: `root`,
+  key: `settings`,
+  version: 1,
   storage: storage,
   whitelist: [`settings`],
+  migrate: (state) => {
+    return Promise.resolve(
+      state
+        ? {
+            ...state,
+            settings: {
+              ...initialSettings,
+              ...state.settings,
+            },
+          }
+        : state,
+    );
+  },
 };
 
 const loggerConfig = {
