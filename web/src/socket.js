f875cac945e54d0d7d3570756390320a962646e4
Kadauchi <kadauchi@gmail.com>
2019-05-14 17:21:20 -0700

    Changed the port.

--- web/src/socket.js
+++ web/src/socket.js
@@ -3,7 +3,7 @@ import socketIoClient from 'socket.io-client';
 
 import { connectedUpdate, hitAdded, hitHistory } from './redux/actions';
 
-const socket = socketIoClient(`http://127.0.0.1:3001`);
+const socket = socketIoClient(`http://127.0.0.1:8080`);
 
 socket.on(`connections`, (data) => {
   store.dispatch(connectedUpdate(data));
