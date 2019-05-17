6d40d13ac4941cebb73fc4c9b07ba4ea8339137b
Kadauchi <kadauchi@gmail.com>
2019-05-17 10:33:21 -0700

    Update Header.jsx

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
