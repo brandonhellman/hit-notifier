e87f949baa0c10dc62cada92d770077fca616fc8
Kadauchi <kadauchi@gmail.com>
2019-05-16 18:01:09 -0700

    Simplified the is functions.

--- server/index.js
+++ server/index.js
@@ -46,22 +46,14 @@ function handleHistory(hit) {
 }
 
 function isMasters(html) {
-  const masters = html.match(/(Qualifications:).+(Masters has been granted)/gi);
+  const mtsMasters = html.match(/(Qualifications:).+(Masters Exists)/gi);
   const mtsPhoto = html.match(/(Qualifications:).+(Photo Moderation Masters Exists)/gi);
-  return Boolean(masters || mtsPhoto);
+  return Boolean(mtsMasters || mtsPhoto);
 }
 
 function isUsOnly(html) {
-  const isUS = html.match(/(Qualifications:).+(Location is US)/gi);
-  const isUS2 = html.match(/(Qualifications:).+(Location is one of:)(.+|)( US)/gi);
   const mtsUs = html.match(/(Qualifications:).+(Location EqualTo US)/gi);
-
-  const isINT = html.match(/(Qualifications:).+(Location is )(?!US)/gi);
-  const isINT2 = html.match(
-    /(Qualifications:).+(Location is one of:)(.+|)( (AF|AX|AL|DZ|AS|AD|AO|AI|AQ|AG|AR|AM|AW|AU|AT|AZ|BS|BH|BD|BB|BY|BE|BZ|BJ|BM|BT|BO|BQ|BA|BW|BV|BR|IO|BN|BG|BF|BI|KH|CM|CA|CV|KY|CF|TD|CL|CN|CX|CC|CO|KM|CG|CD|CK|CR|CI|HR|CU|CW|CY|CZ|DK|DJ|DM|DO|EC|EG|SV|GQ|ER|EE|ET|FK|FO|FJ|FI|FR|GF|PF|TF|GA|GM|GE|DE|GH|GI|GR|GL|GD|GP|GU|GT|GG|GN|GW|GY|HT|HM|VA|HN|HK|HU|IS|IN|ID|IR|IQ|IE|IM|IL|IT|JM|JP|JE|JO|KZ|KE|KI|KP|KR|KW|KG|LA|LV|LB|LS|LR|LY|LI|LT|LU|MO|MK|MG|MW|MY|MV|ML|MT|MH|MQ|MR|MU|YT|MX|FM|MD|MC|MN|ME|MS|MA|MZ|MM|NA|NR|NP|NL|NC|NZ|NI|NE|NG|NU|NF|MP|NO|OM|PK|PW|PS|PA|PG|PY|PE|PH|PN|PL|PT|PR|QA|RE|RO|RU|RW|BL|SH|KN|LC|MF|PM|VC|WS|SM|ST|SA|SN|RS|SC|SL|SG|SX|SK|SI|SB|SO|ZA|GS|SS|ES|LK|SD|SR|SJ|SZ|SE|CH|SY|TW|TJ|TZ|TH|TL|TG|TK|TO|TT|TN|TR|TM|TC|TV|UG|UA|AE|GB|UM|UY|UZ|VU|VE|VN|VG|VI|WF|EH|YE|ZM|ZW))/gi,
-  );
-
-  return Boolean((isUS || isUS2 || mtsUs) && (!isINT || !isINT2));
+  return Boolean(mtsUs);
 }
 
 function handleHIT(html, url, posted) {
