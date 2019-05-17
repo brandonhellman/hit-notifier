9c9aaf17919bd5b9a54e9396c86c2d4840aeba51
Kadauchi <kadauchi@gmail.com>
2019-05-17 10:13:41 -0700

    Resend hit history when the filters update so they can be ran through the filter middleware with the new settings.

--- web/src/redux/middleware/filter.js
+++ web/src/redux/middleware/filter.js
@@ -1,3 +1,5 @@
+import { hitHistory } from '../actions';
+
 function HIT_ADDED(store, next, action) {
   const state = store.getState();
 
@@ -20,12 +22,25 @@ function HIT_HISTORY(store, next, action) {
   return next({ ...action, payload });
 }
 
+function SETTINGS_UPDATE(store, next, action) {
+  const result = next(action);
+
+  if (action.payload.hasOwnProperty(`filterMasters`) || action.payload.hasOwnProperty(`filterUsOnly`)) {
+    const state = store.getState();
+    store.dispatch(hitHistory(state.hits));
+  }
+
+  return result;
+}
+
 export default (store) => (next) => (action) => {
   switch (action.type) {
     case `HIT_ADDED`:
       return HIT_ADDED(store, next, action);
     case `HIT_HISTORY`:
       return HIT_HISTORY(store, next, action);
+    case `SETTINGS_UPDATE`:
+      return SETTINGS_UPDATE(store, next, action);
     default:
       return next(action);
   }
