afa4d4e5849f0e613306a94e6ef04bab11a70def
Kadauchi <kadauchi@gmail.com>
2019-05-18 16:22:34 -0700

    Create getProject.js

---
+++ server/functions/getProject.js
@@ -0,0 +1,55 @@
+const cheerio = require(`cheerio`);
+const { JSDOM } = require(`jsdom`);
+
+function getRequesterId(html, frag) {
+  return null;
+}
+
+function getRequesterName(html, frag) {
+  return null;
+}
+
+function getTitle(html, frag) {
+  const mtsMatch = [...frag.querySelectorAll(`b`)].find((b) => b.textContent === `Title:`);
+
+  if (mtsMatch && mtsMatch.nextElementSibling && mtsMatch.nextElementSibling.textContent) {
+    return mtsMatch.nextElementSibling.textContent.trim();
+  }
+
+  return null;
+}
+
+function getDescription(html, frag) {
+  const mtsMatch = [...frag.querySelectorAll(`b`)].find((b) => b.textContent === `Description:`);
+
+  if (mtsMatch && mtsMatch.nextSibling && mtsMatch.nextSibling.nodeValue) {
+    return mtsMatch.nextSibling.nodeValue.trim();
+  }
+
+  return null;
+}
+
+function getReward(html, frag) {
+  const mtsMatch = html.match(/Reward:.+?([0-9.]+)/) ? html.match(/Reward:.+?([0-9.]+)/)[1] : null;
+
+  if (mtsMatch) {
+    return Number(mtsMatch[0]);
+  }
+
+  return null;
+}
+
+function getProject(html) {
+  const frag = JSDOM.fragment(html);
+
+  return {
+    hit_set_id: null,
+    requester_id: getRequesterId(html, frag),
+    requester_name: getRequesterName(html, frag),
+    title: getTitle(html, frag),
+    description: getDescription(html, frag),
+    reward: getReward(html, frag),
+  };
+}
+
+module.exports = getProject;
